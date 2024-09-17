583dc88143769564c96a9593dee4bc56a63f92f8
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2024-09-16 20:26:05 -0700

    replace counter

--- opendbc/car/tesla/carcontroller.py
+++ opendbc/car/tesla/carcontroller.py
@@ -44,9 +44,8 @@ class CarController(CarControllerBase):
     if self.CP.openpilotLongitudinalControl and self.frame % 2 == 0:
       acc_state = CS.das_control["DAS_accState"]
       target_accel = clip(actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
-      target_speed = 0
-      counter = CS.das_control["DAS_controlCounter"]
-      can_sends.append(self.tesla_can.create_longitudinal_commands(acc_state, target_speed, target_accel, counter))
+      target_speed = 0 if target_accel < 0 else 40
+      can_sends.append(self.tesla_can.create_longitudinal_commands(acc_state, target_speed, target_accel, (self.frame // 2) % 8))
 
     # Cancel on user steering override, since there is no steering torque blending
     if hands_on_fault:
